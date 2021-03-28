b6938df3e099bd8753b52aea4220141a86cddf34
J2b2590 <J2b2590@gmail.com>
2021-03-28 06:53:38 -0500

    added to styling to counter div

--- src/Counter.js
+++ src/Counter.js
@@ -4,7 +4,15 @@ const Counter = (props) => {
   console.log(props, "count");
 
   return (
-    <div style={{ margin: "0", textAlign: "center" }}>
+    <div
+      style={{
+        margin: "0 auto",
+        marginTop: "25%",
+        textAlign: "center",
+        width: "50%",
+        border: "3px solid green",
+      }}
+    >
       <h1>{props.count}</h1>
       <button onClick={() => props.UpCount()}>UP</button>
       <button onClick={() => props.DownCount()}>Down</button>
